aeb8b6c6bc0f9ba6293e88f4f22b5405a810075d
Enrique Orea <orea_96@gmail.com>
2023-06-23 10:06:45 -0600

    FIX UNDEFINED

--- app/sedes/[place]/page.tsx
+++ app/sedes/[place]/page.tsx
@@ -18,7 +18,11 @@ const PlacePage = ({ params }: IProps) => {
     `/api/place?place=${params.place}`,
     fetcher
   );
-  console.log(data);
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>Error...</div>;
+
   return (
     <div>
       <h1>{data.name}</h1>
