976fa80f668c36d1296323308250de1db5577f88
Enrique Orea <orea_96@gmail.com>
2023-06-23 09:55:00 -0600

    FETCH DATA

--- app/sedes/[place]/page.tsx
+++ app/sedes/[place]/page.tsx
@@ -1,7 +1,29 @@
+"use client";
 import React from "react";
+import useSWR from "swr";
 
-const PlacePage = () => {
-  return <div>page</div>;
+interface IProps {
+  params: {
+    place: string;
+  };
+}
+
+const fetcher = (...args: [RequestInfo, RequestInit?]) =>
+  fetch(...args).then((res) => res.json());
+
+const PlacePage = ({ params }: IProps) => {
+  console.log(params.place);
+
+  const { data, error, isLoading } = useSWR(
+    `/api/place?place=${params.place}`,
+    fetcher
+  );
+  console.log(data);
+  return (
+    <div>
+      <h1>{data.name}</h1>
+    </div>
+  );
 };
 
 export default PlacePage;
---
+++ constants/index.ts
@@ -0,0 +1,30 @@
+export const sedes = [
+  {
+    id: 1,
+    name: "Parque Juarez",
+  },
+  {
+    id: 2,
+    name: "Galeria de arte contemporaneo",
+  },
+  {
+    id: 3,
+    name: "Casa del lago",
+  },
+  {
+    id: 4,
+    name: "Xallitic",
+  },
+  {
+    id: 5,
+    name: "Cafetería de la 4 Regiones",
+  },
+  {
+    id: 6,
+    name: "Hotel Holiday Inn",
+  },
+  {
+    id: 7,
+    name: "Bajos del Palacio Municipal y Federal",
+  },
+];
