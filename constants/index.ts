31d9197797e81eaf4e84f802850c7b7ff14092df
Enrique Orea <orea_96@gmail.com>
2023-06-25 10:58:03 -0600

    CHANGE IMPLEMENTATION ABOUT DATA

--- constants/index.ts
+++ constants/index.ts
@@ -1,30 +1,177 @@
-export const sedes = [
+import { Sede } from "@/models/sedes";
+
+export const sedes: Sede[] = [
   {
-    id: 1,
-    name: "Parque Juarez",
+    id: "parque_juarez",
+    name: "Parque Juárez",
+    image: "/parque_juarez.jpg",
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15042.049798398597!2d-96.92248142081455!3d19.51960098935778!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db2dfed9298c2b%3A0x390bc4738c67f0b9!2sParque%20Ju%C3%A1rez!5e0!3m2!1ses-419!2smx!4v1687540276948!5m2!1ses-419!2smx",
+    description: [
+      "Anclado en la historia y enriquecido por la vibrante cultura xalapeña, el Parque Juárez te espera, para ser testigo de la celebración más aromática del año, el Festival Café Veracruz. Este parque, fundado en 1892 sobre las ruinas del antiguo Convento de San Francisco, se erige como un emblema de la ciudad y será el escenario principal de nuestro festival",
+      "Ven y sumérgete en una experiencia única, donde el aroma del café recién molido se mezcla con el aire fresco de este histórico parque. Descubre la majestuosidad de Xalapa, mientras te deleitas con los sabores inigualables del café veracruzano, en un lugar donde la grandeza, naturaleza y cultura confluyen. ¡Te esperamos en el Festival Café Veracruz en el Parque Juárez!",
+    ],
+    program: [
+      {
+        fecha: "30 de junio",
+        hora: "10:00 h",
+        descripcion: "Apertura de stands",
+      },
+      {
+        fecha: "30 de junio",
+        hora: "11:00 a 12:00 h",
+        descripcion: "Participación de soprano Grethel Amor",
+      },
+      {
+        fecha: "30 de junio",
+        hora: "12:30 a 13:30 h",
+        descripcion: "Ballet folklórico UV",
+      },
+      {
+        fecha: "30 de junio",
+        hora: "17:00 a 17:30 h",
+        descripcion:
+          "Inauguración (corte de listón y recorrido por los stands)",
+      },
+      {
+        fecha: "30 de junio",
+        hora: "17:30 a 18:30 h",
+        descripcion: "Orquesta Sinfónica del H. Ayuntamiento de Xalapa",
+      },
+      {
+        fecha: "30 de junio",
+        hora: "17:00 a 18:00 h",
+        descripcion: "Hora feliz del café",
+      },
+      {
+        fecha: "1 de julio",
+        hora: "10:00 h",
+        descripcion: "Apertura de stands",
+      },
+      {
+        fecha: "1 de julio",
+        hora: "11:00 a 12:00 h",
+        descripcion: "Participación del cantante Enrique Santoyo",
+      },
+      {
+        fecha: "1 de julio",
+        hora: "16:00 a 17:00 h",
+        descripcion: "Tlen-Huicani Arpa UV",
+      },
+      {
+        fecha: "1 de julio",
+        hora: "17:00 a 18:00 h",
+        descripcion: "Hora feliz del café",
+      },
+      {
+        fecha: "1 de julio",
+        hora: "19:00 a 20:00 h",
+        descripcion: "Orquesta de Salsa de la UV",
+      },
+      {
+        fecha: "2 de julio",
+        hora: "10:00 h",
+        descripcion: "Apertura de stands",
+      },
+      {
+        fecha: "2 de julio",
+        hora: "11:00 a 12:00 h",
+        descripcion: "Mariachi universitario UV",
+      },
+      {
+        fecha: "2 de julio",
+        hora: "12:00 a 14:00 h",
+        descripcion: "Premiación del Concurso de Café 2023",
+      },
+      {
+        fecha: "2 de julio",
+        hora: "17:00 a 18:00 h",
+        descripcion: "Orquesta Pauta Nueva (Danzonera Municipal)",
+      },
+    ],
   },
+  //Xallitic
   {
-    id: 2,
-    name: "Galeria de arte contemporaneo",
-  },
-  {
-    id: 3,
-    name: "Casa del lago",
-  },
-  {
-    id: 4,
+    id: "xallitic",
     name: "Xallitic",
+    image: "/xallitic.jpg",
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d558.9626725227697!2d-96.92268610370174!3d19.53140767339551!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db2e007640386d%3A0xdd2547183dd03ddb!2sPlaza%20y%20Puente%20de%20Xallitic!5e0!3m2!1ses-419!2smx!4v1687547224858!5m2!1ses-419!2smx",
+    description: [
+      "Descubre la riqueza histórica y cultural de Xalapa en el icónico barrio de Xallitic. Desde la época de la tribu Totonaca hasta nuestros días, Xallitic se ha mantenido como un faro de historia y tradición en medio del bullicio de la ciudad.",
+      "¡Ven al Festival Café Veracruz y sumérgete en una experiencia cultural sin precedentes en Xallitic! Admira el templete y los lavaderos que aún se utilizan, y déjate llevar por los pasadizos bajo el puente, donde se combinan la historia, el arte y la cultura de Xalapa. ¡No pierdas la oportunidad de sumergirte en una experiencia llena de gran café y cultura local en el corazón de Xalapa!",
+    ],
+    program: [],
   },
+  //La Casa del Lago
   {
-    id: 5,
-    name: "Cafetería de la 4 Regiones",
+    id: "casa_lago",
+    name: "La Casa del Lago",
+    image: "/casa_lago.jpg",
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d940.105004514078!2d-96.92474533202454!3d19.523573203628374!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db2df93e6b7b17%3A0xefea20d041640bb8!2sUV%20-%20Casa%20del%20Lago!5e0!3m2!1ses-419!2smx!4v1687547449173!5m2!1ses-419!2smx",
+    description: [
+      "Imagina un lugar donde la rica historia se mezcla con la vibrante cultura contemporánea, ese lugar es la Casa del Lago. Con casi dos siglos de historia, este centro cultural universitario es un emblema de Xalapa y será uno de los escenarios del Festival Café Veracruz.",
+
+      "Este recinto ha evolucionado a través del tiempo, adaptándose a las necesidades culturales de la ciudad, siendo hoy un espacio donde se promueve el arte y la cultura. Te invitamos a descubrir la magia de la Casa del Lago, un lugar donde la herencia histórica de Xalapa y la efervescencia del café veracruzano se entrelazan. ¡Ven y sumérgete en la experiencia del Festival Café Veracruz en la Casa del Lago!",
+    ],
+    program: [],
   },
+  //galeria de arte contemporaneo
   {
-    id: 6,
-    name: "Hotel Holiday Inn",
+    id: "galeria_arte",
+    name: "Galería de Arte Contemporáneo",
+    image: "/galeria_arte.jpg",
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d664.7309657613928!2d-96.91552726119912!3d19.529323110293216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db3202288cda21%3A0x3e45e7a7580e9fe0!2sGaler%C3%ADa%20de%20Arte%20Contempor%C3%A1neo!5e0!3m2!1ses-419!2smx!4v1687547581149!5m2!1ses-419!2smx",
+    description: [
+      "La Galería de Arte Contemporáneo de Xalapa, una joya arquitectónica del siglo XVIII, será una de las sedes cautivadoras del Festival Café Veracruz. Anteriormente conocida como el Mesón de San Bernardo, este espacio ha sido un punto de encuentro para viajeros y comerciantes, y ahora alberga algunas de las expresiones artísticas más vanguardistas de Veracruz.",
+      "Te invitamos a ser parte de esta celebración única de café y cultura en la Galería de Arte Contemporáneo. Sumérgete en la riqueza del arte contemporáneo mientras saboreas las delicadas notas de los cafés veracruzanos. ¡Ven y vive una experiencia inolvidable en el Festival Café Veracruz en la Galería de Arte Contemporáneo!",
+    ],
+    program: [],
   },
+  //cafeteria 4 regiones
   {
-    id: 7,
-    name: "Bajos del Palacio Municipal y Federal",
+    id: "cafeteria_4",
+    name: "Cafetería 4 Regiones",
+    image: "/cafeteria_4.jpg",
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235.01976231569049!2d-96.92022345770027!3d19.528033950275862!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db33daf6766f75%3A0x5f981aa5337c60e0!2s4%20Regiones!5e0!3m2!1ses-419!2smx!4v1687547892207!5m2!1ses-419!2smx",
+    description: [
+      "La Cafetería 4 Regiones, ubicada en el corazón de Xalapa, será una de las sedes del Festival Café Veracruz. Este espacio, que anteriormente fue un restaurante, se ha convertido en un punto de encuentro para los amantes del café y la cultura. ",
+      "Ven y descubre la magia de la Cafetería 4 Regiones, donde la historia y la cultura se mezclan con el aroma del café veracruzano. ¡Te esperamos en el Festival Café Veracruz en la Cafetería 4 Regiones!",
+    ],
+    program: [],
+  },
+  //Hotel holiday inn Xalapa
+  {
+    id: "holiday_inn",
+    name: "Hotel Holiday Inn Xalapa",
+    image: "/holiday_inn.jpg",
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d790.5155562160904!2d-96.92288702728243!3d19.526715053814687!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db2e2dfb6f9b41%3A0x4d9846fd7a8336f4!2sHoliday%20Inn%20Express%20Xalapa%2C%20an%20IHG%20Hotel!5e0!3m2!1ses-419!2smx!4v1687548051029!5m2!1ses-419!2smx",
+    description: [
+      "El Hotel Holiday Inn Xalapa, ubicado en el corazón de la ciudad, será una de las sedes del Festival Café Veracruz. Este hotel, que anteriormente fue un restaurante, se ha convertido en un punto de encuentro para los amantes del café y la cultura. ",
+      "Ven y descubre la magia del Hotel Holiday Inn Xalapa, donde la historia y la cultura se mezclan con el aroma del café veracruzano. ¡Te esperamos en el Festival Café Veracruz en el Hotel Holiday Inn Xalapa!",
+    ],
+    program: [],
+  },
+  //Bajos del Palacio de Gobierno
+  {
+    id: "palacio_gobierno",
+    name: "Bajos del Palacio de Gobierno",
+    image: "/palacio_gobierno.jpg",
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d940.08456743457!2d-96.92323113035015!3d19.527085662830146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db2dff160a22af%3A0xd8c9216a81c27dd6!2sPalacio%20de%20Gobierno%20del%20Estado%20de%20Veracruz!5e0!3m2!1ses-419!2smx!4v1687548189310!5m2!1ses-419!2smx",
+    description: [
+      "Explora el corazón cívico de Xalapa en los bajos del Palacio Municipal y del Palacio de Gobierno del Estado, ambos íconos históricos en el centro de la ciudad. Con sus majestuosos murales contemporáneos que relatan la historia de México, estos edificios son un testimonio de la rica tradición y patrimonio de Xalapa.",
+      "Únete a nosotros en el Festival Café Veracruz y déjate envolver por la historia y la cultura mientras disfrutas del aroma y los sabores de los cafés de todo Veracruz. ¡Sumérgete en la experiencia única que te ofrecen los bajos del Palacio, entre café, arte e historia!",
+    ],
+    program: [],
+  },
+  //Bajos del Palacio Municipal
+  {
+    id: "palacio_municipal",
+    name: "Bajos del Palacio Municipal",
+    image: "/palacio_municipal.jpg",
+    map: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d940.08456743457!2d-96.92323113035015!3d19.527085662830146!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85db2dfee1507079%3A0x956e0465563b4d6c!2sPalacio%20Municipal%20de%20Xalapa!5e0!3m2!1ses-419!2smx!4v1687548370437!5m2!1ses-419!2smx",
+    description: [
+      "Explora el corazón cívico de Xalapa en los bajos del Palacio Municipal y del Palacio de Gobierno del Estado, ambos íconos históricos en el centro de la ciudad. Con sus majestuosos murales contemporáneos que relatan la historia de México, estos edificios son un testimonio de la rica tradición y patrimonio de Xalapa.",
+      "Únete a nosotros en el Festival Café Veracruz y déjate envolver por la historia y la cultura mientras disfrutas del aroma y los sabores de los cafés de todo Veracruz. ¡Sumérgete en la experiencia única que te ofrecen los bajos del Palacio, entre café, arte e historia!",
+    ],
+    program: [],
   },
 ];
