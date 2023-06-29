23805c1673f31499c43b1dfc69da0b3ad4b22c39
enriqueorea <enriqueorea28@gmail.com>
2023-06-29 14:03:24 -0600

    ADD NEW LOGO FOOTER

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,13 +5,28 @@ import { BsFacebook, BsTwitter } from "react-icons/bs";
 
 const Contact = () => {
   return (
-    <footer className="mt-14 pt-5 mb-10 h-80 max-w-[1200px] mx-auto flex flex-col gap-5 border-t-2 border-light-brown ">
+    <footer className="mt-14 pt-8 mb-10 max-w-[1200px] mx-auto flex flex-col gap-5 border-t-2 border-light-brown ">
       <div className="flex flex-col md:flex-row items-center justify-between">
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col md:flex-row items-center gap-4">
           <Link href={"/"}>
             <Image src={"/logo.png"} width={100} height={80} alt="logo" />
           </Link>
-          <h1>Festival Café Veracruz</h1>
+          <Link href={"http://www.veracruz.gob.mx/AGROPECUARIO/"}>
+            <Image
+              src={"/sedarpa-dorado.png"}
+              width={180}
+              height={100}
+              alt="logo"
+            />
+          </Link>
+          <Link href={"http://www.veracruz.gob.mx/AGROPECUARIO/"}>
+            <Image
+              src={"/sedarpa2-dorado.png"}
+              width={180}
+              height={100}
+              alt="logo"
+            />
+          </Link>
         </div>
         <div className="flex flex-col md:flex-row gap-4 items-center">
           <a className="text-white hover:text-light-brown" href="/#sedes">
@@ -25,15 +40,7 @@ const Contact = () => {
           </a>
         </div>
       </div>
-      <div className="flex flex-col md:flex-row gap-5 items-center justify-between">
-        <Link href={"http://www.veracruz.gob.mx/AGROPECUARIO/"}>
-          <Image
-            src={"/sedarpa-dorado.png"}
-            width={180}
-            height={100}
-            alt="logo"
-          />
-        </Link>
+      <div className="flex flex-col md:flex-row gap-5 items-center justify-end mb-5">
         <div className="flex gap-4">
           <Link href={"https://www.facebook.com/SEDARPAVer/"}>
             <BsFacebook className="text-white text-3xl hover:text-light-brown" />
