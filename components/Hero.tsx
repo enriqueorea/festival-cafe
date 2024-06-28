a40f227ef1cf7dd96a107d226cf18c3fae8f9ec5
enrique orea <orea_96@hotmail.com>
2024-06-28 17:38:26 -0600

    fix: iframe

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,16 +29,14 @@ export const Hero = () => {
             /> */}
             <div
               className='relative h-0 overflow-hidden my-8'
-              style={{
-                paddingTop: '56.25%',
-              }}
+              style={{ paddingTop: '56.25%' }}
             >
               <iframe
                 className='absolute top-0 left-0 w-full h-full'
-                src='https://www.youtube.com/embed/PZwfkR7yHKY?si=kzvtHmiOfswUocq7?autoplay=1
-                '
-                title='Festival del Cáfe 2024'
-                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+                src='https://www.youtube.com/embed/PZwfkR7yHKY?autoplay=1'
+                title='Festival del Café 2024'
+                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                allowFullScreen
               ></iframe>
             </div>
 
